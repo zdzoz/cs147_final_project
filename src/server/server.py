32c0c8f79d94cf713f0f65048aca81c43502c749
Oskar Zdziarski <oskarzdz@gmail.com>
2024-06-08 19:04:08 -0700

    add timestamp

--- src/server/server.py
+++ src/server/server.py
@@ -1,19 +1,26 @@
 from flask import Flask, request, jsonify, render_template
+import datetime
 
 app = Flask(__name__)
 
-door_open_count = [0]
+
+def get_time():
+    x = datetime.datetime.now()
+    return x.strftime("%m/%d (%H:%M:%S)")
+
+
+door_open_count = [(0, get_time())]
 
 
 def curr_count():
-    return door_open_count[-1]
+    return door_open_count[-1][0]
 
 
 @app.route("/", methods=["GET", "POST"])
 def door():
     global door_open_count
     if request.method == "POST":
-        door_open_count.append(curr_count() + 1)
+        door_open_count.append((curr_count() + 1, get_time()))
         return jsonify({"message": "Door opened", "door_open_count": curr_count()}), 200
     elif request.method == "GET":
         return jsonify({"door_open_count": curr_count()}), 200
@@ -23,7 +30,7 @@ def door():
 def reset():
     global door_open_count
     if request.method == "POST":
-        door_open_count.append(0)
+        door_open_count.append((0, get_time()))
     else:
         return jsonify({"message": "error"}), 404
     return jsonify({"message": "Count reset", "door_open_count": curr_count()}), 200
@@ -32,7 +39,11 @@ def reset():
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
     global door_open_count
-    return render_template(template_name_or_list="dashboard.html", data=door_open_count)
+    data = list(map(lambda x: x[0], door_open_count))
+    times = list(map(lambda x: x[1], door_open_count))
+    return render_template(
+        template_name_or_list="dashboard.html", times=times, data=data
+    )
 
 
 if __name__ == "__main__":
